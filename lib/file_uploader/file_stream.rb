9aa5a8e36646d659865a6d9daa79a1479b6128cd
Mann <chanmannlim@gmail.com>
2011-04-08 17:18:01 -0700

    fix last broken bytes as file_stream is eventually not being closed.

--- lib/file_uploader/file_stream.rb
+++ lib/file_uploader/file_stream.rb
@@ -10,7 +10,8 @@ class FileStream
     def write(stream, dir, filename)
       file = FileMaker.create_file(dir, filename)
       file_stream = FileStream.new(file)
-      file_stream.write stream and file_stream.close
+      file_stream.write stream 
+      file_stream.close
       file.path
     end
   end
